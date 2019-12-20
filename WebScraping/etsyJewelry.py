ec1e771aa3ec3dbe1f26a53ea2141fe5debd977d
Justin Stauffer <justindstauffer@gmail.com>
2019-12-19 21:43:44 -0500

    Add files via upload

--- WebScraping/etsyJewelry.py
+++ WebScraping/etsyJewelry.py
@@ -1,30 +1,24 @@
-import pprint
+from pprint import pprint
 import requests
 from bs4 import BeautifulSoup
+import collections
 pageNum = 1
 resultsArray = []
-
+Item = collections.namedtuple('Item', [
+    'Title',
+    'Price',
+])
 while pageNum < 10:
     URL = 'https://www.etsy.com/search?q=tourmaline+wire+wrap&ref=pagination&page=' + str(pageNum)
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
     results = soup.find_all(class_='v2-listing-card__info')
     for result in results:
+        header = result.find('h2', class_='text-body')
         money = result.find('span', class_='currency-value')
-        resultsArray.append(money.text)
+        res = Item(Title=header.text, Price=money.text)
+        resultsArray.append(res)
     pageNum += 1
 for x in resultsArray:
     print(x)
-print(len(resultsArray))
-# URL = 'https://www.etsy.com/search?q=tourmaline+wire+wrap&ref=pagination&page='
-# page = requests.get(URL)
-
-# soup = BeautifulSoup(page.content, 'html.parser')
-
-# results = soup.find_all(class_='v2-listing-card__info')
-
-# for result in results:
-#     money = result.find('span', class_='currency-value')
-#     print(money.text)
-
-# print(len(results))
+pprint(len(resultsArray))
